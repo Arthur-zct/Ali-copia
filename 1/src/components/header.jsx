c6ffe105a1823493ecf67b221bf43260d71ae128
Arthur <117952514+Arthur-zct@users.noreply.github.com>
2023-04-03 18:25:01 -0300

    Mexendo no header
    Adicionando itens no topo do header e arrumando o css

--- 1/src/components/header.jsx
+++ 1/src/components/header.jsx
@@ -1,14 +1,62 @@
 import Logo from '../img/Logo.png'
+import Style from "../styles/header.module.css"
 
 
 export default function header() {
     return (
         <header>
-            <div>
-                <p>Ola</p>
+            <div className={Style.topo}>
+                <div>
+                    <span>
+                        <a href="/">Vender no aliexpress</a>
+                    </span>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <span>
+                        <a href="/">Ajuda</a>
+                    </span>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <a href="/">Proteção ao consumidor</a>
+                </div>
+
+                <div>
+                    <a href="/">App</a>
+                </div>
+
+                <div>
+                    <span>
+                        <a href="/">brasil/portugues/brl</a>
+                    </span>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <a href="/">Lista de desejos</a>
+                </div>
+
+                <div>
+                    <span>
+                        <a href="/">Minha conta</a>
+                    </span>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+
             </div>
 
-            <div>
+            <div className={Style.Logo_pesquisa}>
                 <img src={Logo} alt="" style={{height: "40px" }} />
             </div>
 
